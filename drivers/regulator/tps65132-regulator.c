ed23cb2210ec7b383d1f6fcb961586882aac5c48
Mohd Faraz <androiabledroid@gmail.com>
2019-08-11 10:03:21 +0000

    drivers: regulator changes for zx55q05
    Signed-off-by: Mohd Faraz <androiabledroid@gmail.com>
    Signed-off-by: Mohd Faraz <mohd.faraz.abc@gmail.com>

--- drivers/regulator/tps65132-regulator.c
+++ drivers/regulator/tps65132-regulator.c
@@ -186,6 +186,9 @@ static int tps65132_regulator_set_voltage(struct regulator_dev *rdev,
 {
 	struct tps65132_regulator *vreg = rdev_get_drvdata(rdev);
 	int val, new_uV, rc;
+#ifdef CONFIG_ZX55Q05_ONLY
+	int i = 0;///zhangwei add
+#endif
 
 	if (!rdev->regmap) {
 		pr_err("regmap not found\n");
@@ -201,14 +204,30 @@ static int tps65132_regulator_set_voltage(struct regulator_dev *rdev,
 		return -EINVAL;
 	}
 	if (!vreg->is_enabled) {
+#ifdef CONFIG_ZX55Q05_ONLY
+		printk(KERN_ERR "%s:JackChen111 verg->is_enabled = %d\n",__func__,vreg->is_enabled);
+#endif
 		vreg->vol_set_val = val;
 		vreg->vol_set_postpone = true;
 	} else {
+#ifdef CONFIG_ZX55Q05_ONLY
+		printk(KERN_ERR "%s:JackChen222 verg->is_enabled = %d vreg->vol_reg = %d val= %d\n", __func__, vreg->is_enabled, vreg->vol_reg, val);
+#endif
 		rc = regmap_write(rdev->regmap, vreg->vol_reg, val);
 		if (rc) {
 			pr_err("failed to write reg %d, rc = %d\n",
 						vreg->vol_reg, rc);
-			return rc;
+#ifdef CONFIG_ZX55Q05_ONLY
+		do
+		{
+			i++;
+			rc = regmap_write(rdev->regmap, vreg->vol_reg, val);
+		}
+		while(rc != 0);
+		printk(KERN_ERR "JackChen OK: i = %d\n", i);
+#else
+		return rc;
+#endif
 		}
 	}
 	vreg->curr_uV = new_uV;
@@ -255,6 +274,9 @@ static int tps65132_regulator_gpio_init(struct tps65132_chip *chip)
 		vreg = &chip->vreg[i];
 		gpio = vreg->en_gpio;
 		flags = vreg->gpio_flags;
+#ifdef CONFIG_ZX55Q05_ONLY
+		printk(KERN_ERR "JackChen:%s gpio[%d] = %d\n", __func__, i, gpio);
+#endif
 		if (gpio_is_valid(gpio)) {
 			rc = devm_gpio_request(chip->dev, gpio, vreg->name);
 			if (rc < 0) {
@@ -486,6 +508,9 @@ static int tps65132_regulator_probe(struct i2c_client *client,
 	struct regulator_desc *rdesc;
 	int i, j, rc;
 
+#ifdef CONFIG_ZX55Q05_ONLY
+	printk(KERN_ERR "JackChen:%s\n", __func__);
+#endif
 	chip = devm_kzalloc(&client->dev, sizeof(struct tps65132_chip),
 							GFP_KERNEL);
 	if (!chip) {
